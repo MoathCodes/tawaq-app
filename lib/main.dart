cb653934bfe2afbe269ab5283cab334b29ea1cee
Moath Altamimi <irondragondev@gmail.com>
2026-09-14 07:03:13 +0300

    fix: Improved the visuals of the hero prayer card

--- lib/main.dart
+++ lib/main.dart
@@ -67,7 +67,8 @@ class AppBootstrap extends ConsumerWidget {
 
     Widget splash() => MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: materialTheme,
+      showSemanticsDebugger:true,
+ theme: materialTheme,
       home: foruiShell(
         child: const FScaffold(
           child: Center(child: FCircularProgress.loader()),
